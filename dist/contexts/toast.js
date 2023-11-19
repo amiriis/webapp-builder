dd740c1b5042accb175ab76be39cfa60d526a915
AmirHossein Mahmoodi <amir.mahmoodi74@gmail.com>
2023-11-19 11:58:07 +0330

    test

--- dist/contexts/toast.js
+++ dist/contexts/toast.js
@@ -1,13 +1,16 @@
 "use strict";
 
+require("core-js/modules/es.weak-map.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.ToastProvider = exports.ToastContext = void 0;
 require("core-js/modules/web.dom-collections.iterator.js");
 require("core-js/modules/es.symbol.description.js");
-var _react = require("react");
+var _react = _interopRequireWildcard(require("react"));
 var _reactToastify = require("react-toastify");
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
 function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
 function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
@@ -40,7 +43,7 @@ const ToastProvider = _ref => {
     warning: [],
     success: []
   });
-  return /*#__PURE__*/React.createElement(ToastContext.Provider, {
+  return /*#__PURE__*/_react.default.createElement(ToastContext.Provider, {
     value: {
       state,
       dispatch
--- src/contexts/toast.js
+++ src/contexts/toast.js
@@ -1,4 +1,4 @@
-import {createContext, useReducer} from "react";
+import React, {createContext, useReducer} from 'react'
 import {toast} from "react-toastify";
 
 export const ToastContext = createContext()
