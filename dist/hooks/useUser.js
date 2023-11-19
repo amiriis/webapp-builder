6d113ab3b178b7cfd951d5e17f409a00d47e4063
AmirHossein Mahmoodi <amir.mahmoodi74@gmail.com>
2023-11-19 12:12:07 +0330

    test

---
+++ dist/hooks/useUser.js
@@ -0,0 +1,37 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = require("react");
+var _user = require("../contexts/user");
+const useUser = () => {
+  const {
+    isAuth,
+    userChangedLanguage,
+    token,
+    user,
+    getUser,
+    clearUser,
+    changeUser,
+    changeAuthState,
+    changeLanguageState,
+    clearToken,
+    setToken
+  } = (0, _react.useContext)(_user.UserContext);
+  return {
+    isAuth,
+    userChangedLanguage,
+    token,
+    user,
+    getUser,
+    clearUser,
+    changeUser,
+    changeAuthState,
+    changeLanguageState,
+    clearToken,
+    setToken
+  };
+};
+var _default = exports.default = useUser;
