9216086ddd45d1f96615c42143d72c5aaacc31b3
AmirHossein Mahmoodi <amir.mahmoodi74@gmail.com>
2023-11-26 13:33:11 +0330

    new version

--- src/utils/layoutManager/index.tsx
+++ src/utils/layoutManager/index.tsx
@@ -1,12 +1,26 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect, useState} from "react";
 import {ILayoutInput} from "../../@types/layout";
+import {resolve} from "app-root-path";
 
-export const LayoutManager: React.FC<React.PropsWithChildren<{ layout: ILayoutInput, layoutList: any }>> = ({
-                                                                                                                children,
-                                                                                                                layout,
-                                                                                                                layoutList
-                                                                                                            }) => {
-    const Component = layoutList[layout?.name] || Fragment
+const layoutList: any = async () => await import(resolve('src/layouts/list'));
+
+export const LayoutManager: React.FC<React.PropsWithChildren<{ layout: ILayoutInput }>> = ({
+                                                                                               children,
+                                                                                               layout,
+                                                                                           }) => {
+    const [list, setList] = useState<any | null>(null);
+
+    useEffect(() => {
+        layoutList().then((_layoutList: any) => {
+            setList(_layoutList)
+        })
+    }, []);
+
+    if (!list) {
+        return null; // or some loading indicator
+    }
+
+    const Component = list[layout?.name] || Fragment
     const props = layout?.props || {}
 
     return (
