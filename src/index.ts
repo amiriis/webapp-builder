10d5002cb04899b52afb2126dded9649c84f21d8
AmirHossein Mahmoodi <amir.mahmoodi74@gmail.com>
2023-11-27 16:15:17 +0330

    new version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import {resolve} from 'app-root-path'
+
+try {
+    global.WitelConfig = require(resolve('witel.config.json'));
+    console.log(WitelConfig)
+} catch (error) {
+    console.error('Error loading layoutList:', error);
+}
+
 export * from './components'
 export * from './contexts'
 export * from './hooks'
