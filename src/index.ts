77ea03d87ffe6a331c613a507ee0a30832b867d8
AmirHossein Mahmoodi <amir.mahmoodi74@gmail.com>
2023-11-27 16:37:07 +0330

    new version

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import * as path from "path";
 
-const jsonFilePath = path.resolve(__dirname, 'witel.config.json');
+const jsonFilePath = path.resolve('@/../witel.config.json');
 
 console.log(jsonFilePath)
-
-try {
-    global.WitelConfig = require(jsonFilePath);
-    console.log(WitelConfig)
-} catch (error) {
-    console.error('Error loading layoutList:', error);
-}
+//
+// try {
+//     global.WitelConfig = require(jsonFilePath);
+//     console.log(WitelConfig)
+// } catch (error) {
+//     console.error('Error loading layoutList:', error);
+// }
 
 export * from './components'
 export * from './contexts'
